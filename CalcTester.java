a1ed74896ee5000955045861179c1491ba12bc0d
YuichiTanaka58 <w.s.imagination@gmail.com>
2024-10-07 10:23:20 +0900

    Update CalcTester.java

--- CalcTester.java
+++ CalcTester.java
@@ -7,16 +7,12 @@ import Display.Display;
 import Input.Input;
 
 public class CalcTester {
-	
-	//10:49元木更新テスト
-	//村田テスト
 
 	private Input input;
 	private Calc calc;
 	private Display display;  // 電卓のGUIを表示する
 
 	public CalcTester() {
-		System.out.println("test"); //松﨑コミットテストです
 		input = new Input(); // 入力のトークン化担当
 		calc = new Calc();   // 計算担当
 		display = new Display(this); // 電卓画面の表示、thisでCalcTesterを渡す
@@ -31,10 +27,25 @@ public class CalcTester {
 	public String handleInput(String expression) {
 		// 数式をトークンに分割
 		List<String> tokens = input.tokenize(expression);
+		
+		//トークンが空の場合、そのまま返す
+		if (tokens.isEmpty()) {
+			return "";
+		}
+		
+		try {
 		// トークンをCalcに渡して計算
 		double result = calc.evaluate(tokens);
-		// 結果を文字列として返す
-		return String.valueOf(result);
+		
+		if(result==(int)result) {//計算結果がキャストされたint型の結果と同じなら
+			return String.valueOf((int)result);//int型にキャストされた結果をString型文字列で返す
+		}else {  
+			//小数点が必要な場合 結果を文字列として返す
+			return String.valueOf(result);
+		}
+		}catch(Exception e) {
+			return "Error";
+		}
 	}
 
 	public static void main(String[] args) {
